9513b2e30541771ce36d0f6de3f274d805d3908e
Thomas Min <thomas.min.git@gmail.com>
2016-09-11 21:39:51 +0900

    Add Xamarin client (WIP)

---
+++ client/xamarin/Subak/Subak/Data.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Subak
+{
+	public class Track
+	{
+		public string Title { get; set; }
+		public string Artist { get; set; }
+		public string File { get; set; }
+	}
+
+	public class Data
+	{
+		public Data()
+		{
+		}
+
+		public static ObservableCollection<Track> TrackList = new ObservableCollection<Track>
+		{
+			new Track{ Title="Title1", Artist="Artist1", File="File1"},
+			new Track{ Title="Title2", Artist="Artist2", File="File2"}
+		};
+	}
+}
---
+++ client/xamarin/Subak/Subak/SubakPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace Subak
+{
+	public partial class SubakPage : ContentPage
+	{
+		public SubakPage()
+		{
+			InitializeComponent();
+			listView.ItemsSource = Data.TrackList;
+		}
+	}
+}
+
